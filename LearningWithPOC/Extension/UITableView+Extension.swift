b281f7166463a9edc0ad31f7dac5827baf189be6
dipakmakwana123 <dipakmakwana123@gmail.com>
2022-07-28 23:15:16 +0530

    - Code Indentation

--- LearningWithPOC/Extension/UITableView+Extension.swift
+++ LearningWithPOC/Extension/UITableView+Extension.swift
@@ -13,9 +13,7 @@ extension UITableView {
         for identifier in identifiers {
             self.register(UINib(nibName: identifier, bundle: .main), forHeaderFooterViewReuseIdentifier: identifier)
         }
-        
     }
-    
     func registerCell(identifiers:[String]){
         for identifier in identifiers {
             self.register(UINib(nibName: identifier, bundle: .main), forCellReuseIdentifier: identifier)
@@ -30,9 +28,7 @@ extension UITableView {
         self.estimatedRowHeight = 100
         self.estimatedSectionHeaderHeight = 44
     }
-    
     func setFooterView() {
- 
         let footerView = UIView(frame:CGRect(x: 0, y: 0, width: self.frame.size.width, height: 100))
         let spiner = UIActivityIndicatorView(style: .medium)
         spiner.startAnimating()
@@ -45,19 +41,17 @@ extension UITableView {
 
 
 extension UITableView {
-
+    
     func setEmptyMessage(_ message: String) {
         let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
         messageLabel.text = message
         messageLabel.textColor = .darkGray
         messageLabel.numberOfLines = 0
         messageLabel.textAlignment = .center
-      //  messageLabel.font =  Font(.installed(.HK_Medium), size: .standard(.h20)).instance
         messageLabel.sizeToFit()
         self.backgroundView = messageLabel
         self.separatorStyle = .none
     }
-
     func restore() {
         self.backgroundView = nil
         self.separatorStyle = .none
